292fdb913465b668747ab3d119d1494b61a340cb
BauceCamille <camille.bauce@etu.unice.fr>
2019-03-26 11:35:39 +0100

    resolve internal error on post login request

--- app/api/login/index.js
+++ app/api/login/index.js
@@ -1,33 +1,32 @@
 const { Router } = require('express');
-const { User } = require('../../model/user.model');
+const { User,Bri,Student } = require('../../models');
 
-const { BRI } = require('../../model/bri.model');
-const { Student } = require('../../model/student.model');
 // const { STUDENT_MOCKS } = require('../../mocks/student.mocks');
 // const { BRI_MOCKS } = require('../../mocks/bri.mocks.json')
 
-const { logger } = require('../../utils/logger');
+const logger = require('../../utils/logger');
 
 const router = new Router();
 
 const getBRIByLoginAndPwd = function (mail, pwd) {
-  return BRI.get().filter(
-    bri => bri.login.equals(mail) && bri.password.equals(pwd),
+  return Bri.get().filter(
+    bri => bri.login === mail && bri.password === pwd,
   );
 };
 
 const getStudentByLoginAndPwd = function (mail, pwd) {
   return Student.get().filter(
-    student => student.mail.equals(mail) && student.password.equals(pwd),
+    student => student.mail === mail && student.password === pwd,
   );
 };
 
 router.get('/', (req, res) => res.status(200).json('ok login'));
 router.post('/', (req, res) => {
+  //res.status(200).json('ok login post');
   try {
     const user = User.create(req.body);
     let resUser = getBRIByLoginAndPwd(user.mail, user.password);
-    logger.log('resUser => ', resUser);
+    //logger.info('resUser => ', resUser);
     if (resUser.length) {
       res.status(201).json(resUser);
     } else {
