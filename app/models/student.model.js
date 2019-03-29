c27f073e5f931161c9cfd71f97178e9bbe2b90fe
BauceCamille <camille.bauce@etu.unice.fr>
2019-03-29 19:02:04 +0100

    added General info fields in student model, coco is  filled mock student.

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -7,7 +7,20 @@ class StudentModel extends BaseModel {
       id: Joi.number().required(),
       mail: Joi.string().required(),
       password: Joi.string().required(),
-      // major: Joi.string().allow('').optional(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      major: Joi.string(), // allow us to know which diploma is being prepared
+      gender: Joi.string(),
+      nationality: Joi.string(),
+      residencePermitExpirationDate: Joi.date(),
+      birthDate: Joi.date(),
+      INE: Joi.string(),
+      studentNumber: Joi.string(),
+      address: Joi.string(),
+      city: Joi.string(),
+      postalCode: Joi.string(),
+      mobilePhoneNumber: Joi.string(),
+      phoneNumber: Joi.string(),
     });
   }
 }
