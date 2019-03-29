e2781affb2bc2e70a31183aceb750742740385ab
BauceCamille <camille.bauce@etu.unice.fr>
2019-03-29 17:47:09 +0100

    login post request is working now

--- app/api/login/index.js
+++ app/api/login/index.js
@@ -1,47 +1,37 @@
 const { Router } = require('express');
 const { Bri, Student } = require('../../models');
 
-// const { STUDENT_MOCKS } = require('../../mocks/student.mocks');
-// const { BRI_MOCKS } = require('../../mocks/bri.mocks.json')
-
-const logger = require('../../utils/logger');
+// const logger = require('../../utils/logger');
 
 const router = new Router();
 
-const getBRIByLoginAndPwd = function (mail, pwd) {
-  const res = Bri.get().filter(
+const getBriByLogin = function (mail, pwd) {
+  return Bri.get().filter(
     bri => bri.mail === mail && bri.password === pwd,
   );
-  logger.log('getBRIbyLoginAndPWd ==> ', res);
-  return res;
 };
 
-const getStudentByLoginAndPwd = function (mail, pwd) {
-  const res = Student.get().filter(
+const getStudentByLogin = function (mail, pwd) {
+  return Student.get().filter(
     student => student.mail.valueOf() === mail.valueOf()
       && student.password.valueOf() === pwd.valueOf(),
   );
-  logger.log(`getStudentByLoginAndPwd ==> ${res}`);
-  return res;
 };
 
-const log = function (message) {
+/* const log = function (message) {
   logger.log(message);
-};
+}; */
 
 router.get('/', (req, res) => res.status(200).json('ok login'));
 router.post('/', (req, res) => {
-  // res.status(200).json('ok login post');
-  log('plop');
   try {
-    let resUser = getBRIByLoginAndPwd(req.body.mail.trim(), req.body.password.trim());
-    // logger.info('resUser => ', resUser);
-    if (resUser.length) {
-      res.status(201).json(resUser);
+    let user = getBriByLogin(req.body.mail.trim(), req.body.password.trim());
+    if (user.length) { // if user is found in BRI base
+      res.status(201).json(user);
     } else {
-      resUser = getStudentByLoginAndPwd(req.body.mail.trim(), req.body.password.trim());
-      if (resUser.length) {
-        res.status(201).json(resUser);
+      user = getStudentByLogin(req.body.mail.trim(), req.body.password.trim());
+      if (user.length) { // if user is found in Student base
+        res.status(201).json(user);
       } else {
         res.status(403).json('Wrong password or login');
       }
