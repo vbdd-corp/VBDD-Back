6de244234dfc3b28a33b50bbe3c006d14a4540c2
Jean-Marie Dormoy <jean-marie.dormoy@etu.univ-cotedazur.fr>
2019-03-29 21:54:47 +0100

    did get and post methods for students/index.js

--- app/api/students/index.js
+++ app/api/students/index.js
@@ -13,9 +13,19 @@ const getStudentById = function (studentId) {
   return Student.getById(studentId);
 };
 
+const getStudentByMajor = function (major) {
+  return Student.get().filter(student => student.major === major);
+};
+
+const getStudentByName = function (name) {
+  return Student.get().filter(
+    student => student.firstName.includes(name) || student.lastName.includes(name),
+  );
+};
+
 router.get('/', (req, res) => res.status(200).json(Student.get()));
-router.get('/by-name/:studentName', (req, res) => res.status(200).json('WIP'));
-router.get('/by-major/:major', (req, res) => res.status(200).json('WIP'));
+router.get('/by-name/:studentName', (req, res) => res.status(200).json(getStudentByName(req.params.studentName)));
+router.get('/by-major/:major', (req, res) => res.status(200).json(getStudentByMajor(req.params.major)));
 
 router.get('/by-id/:studentID', (req, res) => {
   try {
@@ -29,5 +39,19 @@ router.get('/by-id/:studentID', (req, res) => {
   }
 });
 
+/* ce post fonctionne mais l'id est rempli automatiquement par Node.JS
+* probablement avec la date courante. */
+router.post('/', (req, res) => {
+  try {
+    const ticket = Student.create(req.body);
+    res.status(201).json(ticket);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err.extra);
+    } else {
+      res.status(500).json(err);
+    }
+  }
+});
 
 module.exports = router;
