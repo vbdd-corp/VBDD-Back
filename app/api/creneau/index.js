0950058ea9814bb5b969df76298c29b637b46799
BauceCamille <camille.bauce@etu.unice.fr>
2019-05-11 02:45:39 +0200

    get creneau by bri id is done #10

--- app/api/creneau/index.js
+++ app/api/creneau/index.js
@@ -1,8 +1,49 @@
 const { Router } = require('express');
-const { Creneau } = require('../../models');
+const { Creneau, AppointmentType } = require('../../models');
+const logger = require('../../utils/logger');
+
+function logThis(elt) {
+  logger.log(`debug ==> ${elt}`);
+}
 
 const router = Router();
 
+function getAppointmentTypeSafely(appointmentTypeId) {
+  try {
+    return AppointmentType.getById(appointmentTypeId);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      return null;
+    }
+    throw err;
+  }
+}
+
+const attachAppointmentType = (creneau) => {
+  const newCreneau = Object.assign({}, creneau, {
+    appointmentType: getAppointmentTypeSafely(creneau.appointmentTypeId),
+  });
+  delete newCreneau.appointmentTypeId;
+  return newCreneau;
+};
+
 router.get('/', (req, res) => res.status(200).json(Creneau.get()));
+
+router.get('/by-bri/:briId', (req, res) => {
+  try {
+    const creneauList = Creneau.get()
+      .filter(creneau => creneau.briId === parseInt(req.params.briId, 10))
+      .map(creneau => attachAppointmentType(creneau));
+
+    res.status(200).json(creneauList);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      res.status(404).end();
+    } else {
+      logThis(err);
+      res.status(500).json(err);
+    }
+  }
+});
 
 module.exports = router;
